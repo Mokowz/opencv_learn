bafb7454af9df11f41696bc7987a611b601d6361
Ronny Kerosi <ronniemokaya30@gmail.com>
2023-03-06 23:54:41 +0300

    Displayed the image

--- read_img.py
+++ read_img.py
@@ -3,4 +3,9 @@ import cv2
 # Read the image
 img = cv2.imread("dickens-lin-FLSbAX8dheQ-unsplash.jpg", 0)
 
-print(img)
+# print(img)
+
+# Display the image
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
