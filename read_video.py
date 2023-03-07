1b4ab53a53da6b55f32c5d21cb8ba5ff9b0a6781
Ronny Kerosi <ronniemokaya30@gmail.com>
2023-03-07 11:55:05 +0300

    Read and saved my livestream video

--- read_video.py
+++ read_video.py
@@ -3,14 +3,33 @@ import cv2
 # Capture the livestream video
 cap = cv2.VideoCapture(0)
 
+# Create fourcc variable
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+# Output variable (video writer object)
+out = cv2.VideoWriter("me.mp4", fourcc, 30, (640, 480))
+
 # Capture the frames
 while(True):
     ret, frame = cap.read()
 
-    cv2.imshow("webcam", frame)
+    if ret == True:
 
-    if cv2.waitKey(1) == ord("q"):
+        # write the video
+        out.write(frame)
+
+        cv2.imshow("webcam", frame)
+
+        # Get the height and width
+        # print("Width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # print("Height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        if cv2.waitKey(1) == ord("q"):
+            break
+    else:
         break
 
+# Release everything
 cap.release()
+out.release()
 cv2.destroyAllWindows()
