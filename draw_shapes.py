1eec76202ed655e22839389e6ccf5a4ed98ab21e
Ronny Kerosi <ronniemokaya30@gmail.com>
2023-03-07 12:39:48 +0300

    Drawn a rectangle

--- draw_shapes.py
+++ draw_shapes.py
@@ -8,6 +8,9 @@ img = cv2.line(img, (0,0), (640, 480), (20,101,222), 3)
 # Arrowed line
 img = cv2.arrowedLine(img, (0,480), (640,480), (0, 255, 0), 3)
 
+#Draw rectangle 
+img = cv2.rectangle(img, (480, 0), (700, 200), (208,0,0), 5)
+
 # Show the image
 cv2.imshow("Image", img)
 
