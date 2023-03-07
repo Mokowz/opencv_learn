4fe8ebae6de30666c561a9adc4798574023665e0
Ronny Kerosi <ronniemokaya30@gmail.com>
2023-03-07 12:32:19 +0300

    Added an arrowed line

--- draw_shapes.py
+++ draw_shapes.py
@@ -5,6 +5,8 @@ img = cv2.imread("dickens-lin-FLSbAX8dheQ-unsplash.jpg", 1)
 
 # Draw the line
 img = cv2.line(img, (0,0), (640, 480), (20,101,222), 3)
+# Arrowed line
+img = cv2.arrowedLine(img, (0,480), (640,480), (0, 255, 0), 3)
 
 # Show the image
 cv2.imshow("Image", img)
