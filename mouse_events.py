2c805f561f553fee939db81d086838950aa82d9b
Ronny Kerosi <ronniemokaya30@gmail.com>
2023-03-09 12:36:53 +0300

    Added more mouse events

--- mouse_events.py
+++ mouse_events.py
@@ -1,30 +1,30 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("dickens-lin-FLSbAX8dheQ-unsplash.jpg")
-
+# img = cv2.imread("dickens-lin-FLSbAX8dheQ-unsplash.jpg")
+img = np.zeros([512,512,3], np.uint8)
+points = []
 cv2.imshow("Image", img)
 
 # Callback function
 def mouse_click(event, x, y, flags, params):
+    # Create a lnie between two points
     if event == cv2.EVENT_LBUTTONDOWN:
-        font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-        strXY = str(x) +  ", " + str(y)
-        # strXY = "Love"
-        cv2.putText(img, strXY, (x,y), font, .5, (50,210, 100), 2)
+        cv2.circle(img, (x,y), 3, (0,200,40), -1)
+        points.append((x,y))
 
-        cv2.imshow("Image", img)
-    
+        if len(points) >= 2:
+            cv2.line(img, points[-1], points[-2], (120,250, 50), 3)
+        
+
     if event == cv2.EVENT_RBUTTONDOWN:
-        blue = img[y, x, 0]
-        green = img[y, x, 1]
-        red = img[y, x, 2]
         font = cv2.FONT_HERSHEY_SIMPLEX
-        strBGR = str(red) +  ", " + str(green) + ", " + str(blue)
-        # strcolor = "Love"
-        cv2.putText(img, strBGR, (x,y), font, .5, (42,88, 200), 2)
-
-        cv2.imshow("Image", img)
+        text = "Done"
+        cv2.putText(img, text, (x,y), font, 1, (0,0,255), 2)
+    
+    cv2.imshow("Image", img)
+    
+    
 
 
 cv2.setMouseCallback("Image", mouse_click)
